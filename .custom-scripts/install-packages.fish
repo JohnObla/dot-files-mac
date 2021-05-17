2d04ea4b2062e254140ba77f91cc2bc743aa7e1a
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-17 18:48:47 +0100

    remove manual language server install

--- .custom-scripts/install-packages.fish
+++ .custom-scripts/install-packages.fish
@@ -24,16 +24,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 # https://github.com/junegunn/vim-plug/issues/225#issuecomment-583541912
 nvim --headless +PlugInstall +qall
 
-# install lsp servers
-npm install -g typescript typescript-language-server
-npm install -g bash-language-server
-npm install -g vscode-css-languageserver-bin
-npm install -g dockerfile-language-server-nodejs
-npm install -g graphql-language-service-cli
-npm install -g vscode-html-languageserver-bin
-npm install -g vscode-json-languageserver
-npm install -g vim-language-server
-npm install -g yaml-language-server
-
 # install better looking git diffs
 npm install -g diff-so-fancy
+
