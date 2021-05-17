d83cfd546d719da3487a28bd635dd5c0055d6456
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-17 22:21:21 +0100

    use lua keybindings

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,8 +1,11 @@
 " general settings
 source $HOME/.config/nvim/general.vimrc
 
-" custom key bindings
-source $HOME/.config/nvim/keys.vimrc
+lua << EOF
+-- key bindings
+require('keys')
+
+EOF
 
 " plugin manager
 source $HOME/.config/nvim/plugins.vimrc
@@ -18,3 +21,4 @@ source $HOME/.config/nvim/nvimtree.vimrc
 
 " gitsigns config
 source $HOME/.config/nvim/gitsigns.vimrc
+
---
+++ config/nvim/lua/keys.lua
@@ -0,0 +1,29 @@
+-- custom key bindings
+
+local map = require('utils').map
+
+-- set leader to space
+vim.g.mapleader = " "
+map('n','<space>','<nop>') -- ignore usual space action
+
+-- reload vimrc
+map('n','<leader>rv',':source $MYVIMRC<CR>')
+
+-- file save and file quit
+map('n','<leader><leader>w',':w<CR>')
+map('n','<leader><leader>q',':q<CR>')
+
+-- makes window navigation easier
+map('n','<C-h>','<C-w>h')
+map('n','<C-j>','<C-w>j')
+
+map('n','<C-k>','<C-w>k')
+map('n','<C-l>','<C-w>l')
+
+-- rebind horizontal split
+map('n','<C-w>h','<C-w>s')
+
+-- buffer next shortcut
+map('n','<leader>bn ',':bn<CR>')
+map('n','<leader>bp ',':bp<CR> ')
+
