1007a28caa9dcd7f749f7a7ed9b735c4956c0b5e
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-18 15:32:07 +0100

    add completion plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -105,4 +105,43 @@ return require('packer').startup(function()
       end
     end
   }
+  -- code completion
+  use {
+    'hrsh7th/nvim-compe',
+    config = function()
+      vim.o.completeopt = "menuone,noselect"
+      require'compe'.setup {
+        enabled = true;
+        autocomplete = true;
+        debug = false;
+        min_length = 1;
+        preselect = 'enable';
+        throttle_time = 80;
+        source_timeout = 200;
+        incomplete_delay = 400;
+        max_abbr_width = 100;
+        max_kind_width = 100;
+        max_menu_width = 100;
+        documentation = true;
+
+        source = {
+          path = true;
+          buffer = true;
+          calc = true;
+          nvim_lsp = true;
+          nvim_lua = true;
+          vsnip = true;
+          ultisnips = true;
+        };
+      }
+      vim.api.nvim_exec([[
+        inoremap <silent><expr> <C-Space> compe#complete()
+        inoremap <silent><expr> <CR>      compe#confirm('<CR>')
+        inoremap <silent><expr> <C-e>     compe#close('<C-e>')
+        inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
+        inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
+      ]])
+    end
+  }
 end)
+
