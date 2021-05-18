8d65a7a8c3a1924bc40c3b566efb16a1662b4d42
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-18 15:41:16 +0100

    use init.lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- key bindings
+require('keys')
+-- general settings
+require('general')
+-- plugin manager
+require('plugins')
+
+-- treesitter config
+vim.cmd 'source $HOME/.config/nvim/treesitter.vimrc'
+-- nvimtree config
+vim.cmd 'source $HOME/.config/nvim/nvimtree.vimrc'
