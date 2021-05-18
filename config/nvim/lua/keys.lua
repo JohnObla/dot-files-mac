006dbb10c22cfda064346a077b1901a3b8dde50f
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-18 08:20:52 +0100

    remove utils file

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -1,6 +1,10 @@
 -- custom key bindings
 
-local map = require('utils').map
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 
 -- set leader to space
 vim.g.mapleader = " "
