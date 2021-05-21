3e4f1ff6a7b2552f379f5615ef057f32a302a444
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-21 18:19:18 +0100

    telescope searches hidden files

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -5,12 +5,15 @@ function module.setup()
     if opts then options = vim.tbl_extend('force', options, opts) end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
   end
+
+  local find_command = "{ 'rg', '--files', '--hidden', '-g', '!node_modules/**', '-g', '!.git/**', }"
+
+
   -- Find files using Telescope command-line sugar.
-  map('n','<leader>ff','<cmd>Telescope find_files<cr>')
-  map('n','<leader>fg','<cmd>Telescope live_grep<cr>')
+  map('n','<leader>ff','<cmd>lua require(\'telescope.builtin\').find_files({find_command = ' .. find_command .. ' })<cr>')
+  map('n','<leader>fg','<cmd>lua require(\'telescope.builtin\').live_grep({find_command = ' .. find_command .. ' })<cr>')
   map('n','<leader>fb','<cmd>Telescope buffers<cr>')
   map('n','<leader>fh','<cmd>Telescope help_tags<cr>')
-
 end
 
 return module
