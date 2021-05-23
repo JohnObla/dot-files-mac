7ecf7648c0f9d353efa209fd4bf6994128e237db
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-05-23 13:36:52 +0100

    livegrep seearches dot files too

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -7,11 +7,27 @@ function module.setup()
   end
 
   local find_command = "{ 'rg', '--files', '--hidden', '-g', '!node_modules/**', '-g', '!.git/**', }"
+  require('telescope').setup{
+    defaults = {
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+        '--hidden',
+        '--glob=!node_modules/**',
+        '--glob=!.git/**',
+      },
+    }
+  }
 
 
   -- Find files using Telescope command-line sugar.
   map('n','<leader>ff','<cmd>lua require(\'telescope.builtin\').find_files({find_command = ' .. find_command .. ' })<cr>')
-  map('n','<leader>fg','<cmd>lua require(\'telescope.builtin\').live_grep({find_command = ' .. find_command .. ' })<cr>')
+  map('n','<leader>fg','<cmd>lua require(\'telescope.builtin\').live_grep()<cr>')
   map('n','<leader>fb','<cmd>Telescope buffers<cr>')
   map('n','<leader>fh','<cmd>Telescope help_tags<cr>')
 end
