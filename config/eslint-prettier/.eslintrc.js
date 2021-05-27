66739e2e8e7f5b0de2104560fe43ff57e76404f9
John Obla <45395888+johnobla@users.noreply.github.com>
2021-05-27 18:01:35 +0100

    improve linter when importing

--- config/eslint-prettier/.eslintrc.js
+++ config/eslint-prettier/.eslintrc.js
@@ -12,11 +12,19 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/style-prop-object': 'off',
     'no-console': 'error',
     'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.tsx']}],
+    'import/extensions': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': [
       'error',
