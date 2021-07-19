7bb2731ff33e8710c693ab0fcac9697573500f2a
John Obla <45395888+johnobla@users.noreply.github.com>
2021-07-19 14:53:14 +0100

    fix lsp.lua error

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -36,10 +36,27 @@ function module.setup()
   -- Use a loop to conveniently call 'setup' on multiple servers and
   -- map buffer local keybindings when the language server attaches
   local servers = {
-    "vim", "yaml", "bash", "html", "lua", "json", "css", "dockerfile", "graphql", "typescript"
-  }
+    "pyright",
+    "rust_analyzer",
+    "tsserver",
+    "vim",
+    "yaml",
+    "bash",
+    "html",
+    "lua",
+    "json",
+    "css",
+    "dockerfile",
+    "graphql",
+    "typescript",
+    }
   for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup { on_attach = on_attach }
+    nvim_lsp[lsp].setup {
+      on_attach = on_attach,
+      flags = {
+      debounce_text_changes = 150,
+      }
+    }
   end
 end
 
