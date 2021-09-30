1f6d0c149c540e98755c8a363d77df1dfe7c0bf1
John Obla <45395888+johnobla@users.noreply.github.com>
2021-09-30 21:44:20 +0100

    remove post spaces

--- config/nvim/lua/config/nvim-tree.lua
+++ config/nvim/lua/config/nvim-tree.lua
@@ -10,18 +10,18 @@ function module.setup()
     update_to_buf_dir   = true,
     -- hijack the cursor in the tree to put it at the start of the filename
     hijack_cursor       = true,
-    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
     update_cwd          = true,
     -- show lsp diagnostics in the signcolumn
     lsp_diagnostics     = true,
-  
+
     view = {
       -- width of the window, can be either a number (columns) or a string in `%`
       width = 40,
      -- if true the tree will resize itself after opening a file
       auto_resize = true,
     }
-  } 
+  }
 end
 
 return module
