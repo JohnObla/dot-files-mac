5834bcc47f961aff26b98473c5bcebdcc0219f40
John Obla <jo.github@outlook.com>
2022-01-20 18:26:32 +0000

    add postgres.app to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,3 +27,6 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 
 # add nx cloud env variable
 set_nx_cloud_env_variable
+
+# add postgres.app to path
+fish_add_path /Applications/Postgres.app/Contents/Versions/14/bin
