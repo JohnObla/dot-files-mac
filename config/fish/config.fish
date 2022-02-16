402f2b9d488b2506631b914314e28f4cbe0eb9c2
John Obla <jo.github@outlook.com>
2022-02-16 11:37:56 +0000

    remove n config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,6 @@
 # set default editor to neovim 
 set -gx EDITOR nvim
 
-# Added by n-install (see http://git.io/n-install-repo).
-# Original below
-# export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  
-set N_PREFIX "$HOME/n"
-fish_add_path $N_PREFIX/bin
-
 # set long path for prompt
 set -g theme_short_path no
 
